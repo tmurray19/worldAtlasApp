456f464e4adbca1c5e5d09b561015a525e049234
tmurray19 <t.murray19@nuigalway.ie>
2019-03-23 12:27:18 +0000

    user issue fixed

--- app/models/User.java
+++ app/models/User.java
@@ -11,20 +11,46 @@ import play.data.validation.*;
 @Entity
 public class User extends Model {
  	
-	@Required
-	@MinSize(8)
+	//@Required
+	//@MinSize(8)
 	public String name;
 	
-	@Required
-	@Email
+	//@Required
+	//@Email
 	public String email;
 	
-	@Required
-	@MaxSize(1000)
+	//@Required
+	//@MaxSize(1000)
 	public String address;
 	
-	public String toString() {
+	public User(String name, String email, String address) {
+		this.name = name;
+		this.email = email;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
 		return email;
 	}
- 	
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
